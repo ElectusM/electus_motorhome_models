0158ffd8048689f76418916736d0b296f45393e1
ElectusM <145289160+ElectusM@users.noreply.github.com>
2023-09-17 20:47:40 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+author "KRILLE#2428 (ELECTUS SCRIPTS)"
+lua54 'yes'
+
+games {
+    "gta5",
+}
+
+files {
+    'data/**/*.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
